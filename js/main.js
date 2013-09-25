e3f74eefe7a039409eb55522ac815886413a5dad
seandotosull <seandotosull@gmail.com>
2013-09-25 01:04:05 +0100

    Beginning
    The beginning.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('document').ready(function() {
+	var options = { videoId: 'gWOzUzJd6wM', start: 3, increaseVolumeBy: 10, mute:true};
+	$('#wrapper').tubular(options);
+
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
